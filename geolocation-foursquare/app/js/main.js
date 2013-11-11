bd4274acb89b6f26a820dde55de6e510764ff2a2
Guillermo Lopez <willyaranda@mozilla-hispano.org>
2013-11-11 10:41:19 +0100

    Less ugly geolocation demo

--- geolocation-foursquare/app/js/main.js
+++ geolocation-foursquare/app/js/main.js
@@ -1,5 +1,5 @@
 /**
- * DISCLAIMER: DO NOT LOOK AT THIS CODE.
+ * DISCLAIMER: DO NOT LOOK AT THIS CODE. It's pretty ugly.
  */
 /**
  * Foursquare API
@@ -17,10 +17,8 @@ var FOURSQR_ENDPOINT_SEARCH_VENUES = 'https://api.foursquare.com/v2/venues/searc
 var output = document.getElementById('out');
 var loc = navigator.geolocation;
 
-function getVenuesByLoc(latitude, longitude) {
-    out.innerHTML = "<p>Fetching data from Foursquare…</p>";
-
-    function updateOutput(venues) {
+var Foursquare = {
+    updateOutput: function(venues) {
         if (!Array.isArray(venues)) {
             out.innerHTML = "<p>Something bad happened (check console)…</p>";
             return;
@@ -29,70 +27,77 @@ function getVenuesByLoc(latitude, longitude) {
         var ul = document.createElement('ul');
         venues.forEach(function(el) {
             var li = document.createElement('li');
-            li.textContent = el.name;
+            li.textContent = el.name;//JSON.stringify(el.contact);
             ul.appendChild(li);
         });
         fragment.appendChild(ul);
+        out.innerHTML = '<p>And here are the results from 4square:</p>'
         out.appendChild(fragment);
-    };
+    },
 
-    var url = FOURSQR_ENDPOINT_SEARCH_VENUES + latitude + ',' + longitude;
+    getVenuesByLoc: function(latitude, longitude) {
+        out.innerHTML = "<p>Fetching data from Foursquare…</p>";
 
-    var xhr = new XMLHttpRequest({mozSystem: true});
+        var url = FOURSQR_ENDPOINT_SEARCH_VENUES + latitude + ',' + longitude;
 
-    xhr.open("GET", url, true);
-    xhr.responseType = 'json';
+        var xhr = new XMLHttpRequest({mozSystem: true});
 
-    xhr.onload = function () {
-      console.log('XHR fired onload - ' + xhr.status);
-      if (xhr.status === 200 || xhr.status === 0) {
-        updateOutput(xhr.response.response.venues);
-      } else {
-        out.innerHTML = '<p>Something bad happened (status=' + xhr.status + ')…</p>';
-        console.error('Status=' + xhr.status);
-      }
-    };
+        xhr.open("GET", url, true);
+        xhr.responseType = 'json';
 
-    xhr.onerror = function(e) {
-        console.error('Status=' + xhr.status);
-    };
+        xhr.onload = function () {
+          console.log('XHR fired onload - ' + xhr.status);
+          if (xhr.status === 200 || xhr.status === 0) {
+            Foursquare.updateOutput(xhr.response.response.venues);
+          } else {
+            out.innerHTML = '<p>Something bad happened (status=' + xhr.status + ')…</p>';
+            console.error('Status=' + xhr.status);
+          }
+        };
 
-    xhr.send();
-}
+        xhr.onerror = function(e) {
+            console.error('Status=' + xhr.status);
+        };
 
-function testGeo() {
-    if (!loc) {
-        console.log('Oh dear…');
-        button.disabled = true;
-        return;
+        xhr.send();
     }
-}
+};
 
-function getLocation() {
-    function onSuccess(position) {
-        var latitude  = position.coords.latitude;
-        var longitude = position.coords.longitude;
+var Geolocation = {
+    testGeo: function() {
+        if (!loc) {
+            console.log('Oh dear…');
+            button.disabled = true;
+            return;
+        }
+    },
 
-        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
+    getLocation: function() {
+        function onSuccess(position) {
+            var latitude  = position.coords.latitude;
+            var longitude = position.coords.longitude;
 
-        var img = new Image();
-        img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
+            output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
 
-        output.appendChild(img);
+            var img = new Image();
+            img.src = "http://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
 
-        setTimeout(function() {
-            getVenuesByLoc(latitude, longitude)
-        }, 1000);
+            output.appendChild(img);
+
+            setTimeout(function() {
+                Foursquare.getVenuesByLoc(latitude, longitude);
+            }, 2000);
+        }
+
+        function onError(e) {
+            console.log('ouch' + e.code + '     ' + e.message);
+            output.innerHTML = "Unable to retrieve your location";
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError);
+        output.innerHTML = "<p>Locating…</p>";
     }
-
-    function onError(e) {
-        console.log('ouch' + e.code + '     ' + e.message);
-        output.innerHTML = "Unable to retrieve your location";
-    }
-
-    navigator.geolocation.getCurrentPosition(onSuccess, onError);
-    output.innerHTML = "<p>Locating…</p>";
-}
+};
 
 var button = document.getElementById('geolocation');
-button.addEventListener('click', getLocation);
+button.addEventListener('click', Geolocation.getLocation);
